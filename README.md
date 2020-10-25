8d38ba6720eecbfc4247f8b568f14d48ef4185e2
gau <>
2020-10-25 19:49:34 +0900

    0.5.0 First commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# README
+
+ポイント文字をパス上文字に変換するIllustrator用スクリプトです
+
+<div class="fig center" style="margin-bottom: 20px;"><img src="https://www.graphicartsunit.com/saucer/images/convert_to_type_on_a_path/cover.png" alt="イメージ" class="noshadow"></div>
+
+### 更新履歴
+
+* 0.5.0：新規作成
+
+----
+
+### 検証バージョン
+
+* Illustrator CS6、2017〜2021
+
+----
+
+### ダウンロード
+
+* [スクリプトをダウンロードする](https://github.com/gau/convert_to_type_on_a_path/archive/master.zip)
+
+----
+
+### インストール方法
+
+1. ダウンロードしたファイルを解凍します。
+2. 所定の場所に「ポイント文字をパス上文字に変換.jsx」をコピーします。
+3. Illustratorを再起動します。
+4. `ファイル > スクリプト > ポイント文字をパス上文字に変換`と表示されていればインストール成功です。
+
+#### ファイルをコピーする場所
+
+| OS | バージョン | フォルダの場所 |
+|:-----|:-----|:-----|
+| Mac | 全 | /Applications/Adobe Illustrator *(ver)*/Presets/ja_JP/スクリプト/ |
+| 32bit Win | CS5まで | C:\Program Files\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS5, CS6（32bit版） | C:\Program Files (x86)\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS6（64bit版）以降 | C:\Program Files\Adobe\Adobe Illustrator *(ver)* (64 Bit)\Presets\ja_JP\スクリプト\ |
+
+* *(ver)*にはお使いのIllustratorのバージョンが入ります
+
+----
+
+### スクリプトの概要
+
+<div class="fig center"><img src="https://www.graphicartsunit.com/saucer/images/convert_to_type_on_a_path/step1.png" alt="スクリプトの概要" class="noshadow"></div>
+
+通常の手順でパス上文字を作成するのは少し手間がかかります。このスクリプトを使うと、選択したポイント文字をパス上文字に一発で変換します。複数の同時処理にも対応しています。
+
+----
+
+### パスを綺麗な円弧にするスクリプトの紹介
+
+<div class="fig center"><img src="https://www.graphicartsunit.com/saucer/images/convert_to_type_on_a_path/step2.png" alt="テキストパスの円弧を綺麗にするスクリプト" class="noshadow"></div>
+
+テキストパスの円弧を綺麗な左右対称に調整するのは面倒ですが、「[s.h's page](http://shspage.com/)」さんの「[円弧にする](http://shspage.com/aijs/#convert_to_arc)」のスクリプトを使えば、ゆがんだ形のパスを綺麗な円弧にしてくれます。使い方は、パス上文字を選択した状態でスクリプトを実行するだけです。アーチの角度を調整するときに使うととても便利です。s.h's pageさんのスクリプトは、他にも便利なものが多いのでおすすめです。
+
+----
+
+### 注意
+
+* スクリプトの実行前にドキュメントを保存しておくことをお勧めします。
+* ポイント文字が選択されていないときは、警告を表示して処理を中断します。
+* 複合シェイプにしたポイント文字は対象になりません。
+* オブジェクトの種類や構造によって意図しない結果になる可能性もゼロではありません。
+
+----
+
+### 免責事項
+
+* このスクリプトを使って起こったいかなる現象についても制作者は責任を負えません。すべて自己責任にてお使いください。
+* OSのバージョンやその他の状況によって実行できないことがあるかもしれません。もし動かなかったらごめんなさい。
+
+----
+
+### ライセンス
+
+* ポイント文字をパス上文字に変換.jsx
+* Copyright (c) 2020 Toshiyuki Takahashi
+* Released under the MIT license
+* [http://opensource.org/licenses/mit-license.php](http://opensource.org/licenses/mit-license.php)
+* Created by Toshiyuki Takahashi ([Graphic Arts Unit](http://www.graphicartsunit.com/))
+* [Twitter](https://twitter.com/gautt)
---
+++ ポイント文字をパス上文字に変換.jsx
@@ -0,0 +1,102 @@
+(function() {
+
+	// Settings
+	var settings = {
+		'isOneLine' : false,
+		'baseLineOffset' : 1.02,
+		'handleOffset' : [3.5, 4]
+	};
+
+	// Constant
+	const SCRIPT_TITLE = 'ポイント文字をパス上文字に変換';
+	const SCRIPT_VERSION = '0.5.0';
+
+	// Get items
+	var doc = app.activeDocument;  
+	var sel = doc.selection;
+	var targetItems = getTargetItems(sel);
+
+	// Validation
+	if(targetItems.length === 0) {
+		alert('対象のアイテムが見つかりません');
+		return false;
+	} else {
+		mainProcess();
+	}
+
+	// Main process
+	function mainProcess() {
+
+		for (var j = 0; j < targetItems.length; j++) {
+
+			// Get original text frame item.
+			var originalText = targetItems[j];
+
+			// Get bounds.
+			var dummyTexts = [originalText.duplicate(), originalText.duplicate()];
+			dummyTexts[0].contents = '';
+			for (var i = 0; i < originalText.lines[0].length; i++) {
+				originalText.textRanges[i].duplicate(dummyTexts[0]);
+			}
+			for (var i = 0; i < dummyTexts.length; i++) {
+				dummyTexts[i] = dummyTexts[i].createOutline();
+			}
+			var bounds = dummyTexts[1].geometricBounds;
+			bounds[3] = dummyTexts[0].geometricBounds[3];
+			for (var i = 0; i < dummyTexts.length; i++) {
+				dummyTexts[i].remove();
+			}
+
+			// Remove Returns
+			if(settings.isOneLine) {
+				for (var i = 0; i < originalText.textRanges.length; i++) {
+					originalText.textRanges[i].contents = originalText.textRanges[i].contents.replace(/(\r\n|\n|\r|\u0003)/gm, '');
+				}
+			}
+
+			// Create Text path
+			var textPath = doc.pathItems.add();
+			textPath.setEntirePath([[bounds[0], bounds[3] * settings.baseLineOffset],[bounds[2], bounds[3] * settings.baseLineOffset]]);
+
+			// Calculate handle offset
+			var textPathLength = textPath.length;
+			handleOffsets = [textPathLength / settings.handleOffset[0], textPathLength / settings.handleOffset[1]];
+
+			// Move direction of path point
+			var points = textPath.pathPoints;
+			points[0].rightDirection = [points[0].rightDirection[0] + handleOffsets[0] , points[0].rightDirection[1] + handleOffsets[1]];
+			points[1].leftDirection = [points[1].rightDirection[0] - handleOffsets[0] , points[1].rightDirection[1] + handleOffsets[1]];
+
+			// Create Text on a path
+			var textOnAPath = doc.textFrames.pathText(textPath);
+			textOnAPath.move(originalText, ElementPlacement.PLACEBEFORE);
+
+			// Duplicate textrange from original text frame item
+			for (var i = 0; i < originalText.textRanges.length; i++) {
+				originalText.textRanges[i].duplicate(textOnAPath);
+			}
+
+			// Remove original text frame item
+			originalText.remove();
+
+			// Select text on a path
+			textOnAPath.selected = true;
+			textOnAPath.selected = true; // Bug Fixes
+		}
+	}
+
+	// Get target items
+	function getTargetItems(items) {
+		var ti = [];
+		for(var i = 0; i < items.length; i++) {
+			if(items[i].typename === 'TextFrame' && items[i].kind === TextType.POINTTEXT) {
+				ti.push(items[i]);
+			} else if(items[i].typename === 'GroupItem') {
+				ti = ti.concat(getTargetItems(items[i].pageItems));
+			}
+		}
+		return ti;
+	}
+
+}());
+
